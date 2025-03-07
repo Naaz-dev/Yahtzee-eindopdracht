adcf9f67a75a71443a008589a9a888502e5680db
Naaz-dev <N.abouathan@outlook.com>
2025-03-07 16:58:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var dice = [0, 0, 0, 0, 0];
 var locked = [false, false, false, false, false];
 var rollsleft = 3;
+var currentPlayer = 1;
 
-
+var scores = {
+    1: {
+        ones: null, twos: null, threes: null, fours: null, fives: null, sixes: null,
+        threeKind: null, fourKind: null, fullHouse: null, smallStraight: null,
+        largeStraight: null, chance: null, yahtzee: null, sum: 0, bonus: 0, total: 0
+    },
+    2: {
+        ones: null, twos: null, threes: null, fours: null, fives: null, sixes: null,
+        threeKind: null, fourKind: null, fullHouse: null, smallStraight: null,
+        largeStraight: null, chance: null, yahtzee: null, sum: 0, bonus: 0, total: 0
+    }
+};
 
 
 var message = document.getElementById("message");
